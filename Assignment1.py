0f0377a31eeb245e0c56cc16360db091422aa4fb
Makaylamckay9999 <nicolemckay4320@gmail.com>
2026-09-04 20:37:29 -0500

    Create Assignment1.py
    Corrected Spelling mistakes in string literal

---
+++ Assignment1.py
@@ -0,0 +1,10 @@
+print("Hello, World!")
+print("This is incoming coding trainer Kayla! Hoping to capture all coding langauges!" )
+print("Remember I got to catch em all!")
+
+#*I chose this message to remind myself to not be discourgaed or become intimidated by the fear of unknown. 
+#*As of now this is my new path that i'm taking on my coding journey. 
+#*Just Like any other journey, I'll have to learn, brace through rocky terrains and find myself.
+#* Also I may also be influenced by playing so many pokemon fangames at the moment
+
+
